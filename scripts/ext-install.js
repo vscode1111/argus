2aaf7b5b916e4d6fffaa965edbe6ee7d663a269d
Konstantin <scub111g@gmail.com>
2026-05-16 01:45:42 +0300

    Bump to 0.0.40 and drop --skip-license from vsce package

--- scripts/ext-install.js
+++ scripts/ext-install.js
@@ -1,5 +1,5 @@
 const { execSync } = require('child_process');
 const { version } = require('../package.json');
 
-execSync('npx vsce package --allow-missing-repository --skip-license -o dist/', { stdio: 'inherit' });
+execSync('npx vsce package --allow-missing-repository -o dist/', { stdio: 'inherit' });
 execSync(`code.cmd --install-extension dist/argus-${version}.vsix`, { stdio: 'inherit' });
